9c38d4d355ad0f54fccf57aac2ecb84fe5037c8d
toxicsub11 <toxicsub11@gmail.com>
2025-05-09 12:13:29 +0300

    Update menu_scroll.js

--- script/menu_scroll.js
+++ script/menu_scroll.js
@@ -1,6 +1,7 @@
 
             const sideMenu = document.getElementById('sideMenu');
             const stopElement = document.getElementById('stopElement');
+            console.log("ало ало отримали мєню");
 
             if (stopElement) {
                 window.addEventListener('scroll', function() {
@@ -10,8 +11,13 @@
 
                     if (scrollPosition >= stopPosition) {
                         sideMenu.style.opacity = '0';
+setTimeout(() => { sideMenu.style.display = 'none'; }, 500);
+
                     } else {
+                        sideMenu.style.display = 'block'
                         sideMenu.style.opacity = '1';
+
+                        
                     }
                 });
             } else {
